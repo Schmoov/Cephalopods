df41d6983bd100df7dac0f1410967eef0a2a8ef5
Paul Arden <parden@paul-f5Ar1s3.clusters.42paris.fr>
2025-04-03 20:09:41 +0200

    v0.1

--- inc/Cepha.hpp
+++ inc/Cepha.hpp
@@ -7,13 +7,12 @@ class Grid {
 public:
 	int						d;
 	vector<int>				g;
-	vector<vector<bool>>	c;
+	vector<vector<int>>		c;
 
 	Grid();
 	Grid(Grid& og, int pos, int capt);
 
 	void legal();
-	void capture(int pos, int capt);
 	int toInt();
 	int solve();
 	friend std::ostream& operator<<(std::ostream& os, Grid& grid);
--- src/Grid.cpp
+++ src/Grid.cpp
@@ -1,6 +1,6 @@
 #include "../inc/Cepha.hpp"
 
-Grid::Grid() : g(9), c(9, vector<bool>(7, false))
+Grid::Grid() : g(9), c(9, vector<int>(16, 0))
 {
 	cin >> d; cin.ignore();
 	for (int i = 0; i < 9; i++) {
@@ -9,41 +9,12 @@ Grid::Grid() : g(9), c(9, vector<bool>(7, false))
 	legal();
 }
 
-Grid::Grid(Grid& og, int pos, int capt) : g(9), c(9, vector<bool>(7, false))
+Grid::Grid(Grid& og, int pos, int capt) : g(9), c(9, vector<int>(16, 0))
 {
 	d = og.d - 1;
 	g = og.g;
-	capture(pos, capt);
+	g[pos] = og.c[pos][capt];
 	legal();
-}
-
-void Grid::capture(int pos, int capt)
-{
-	switch (capt) {
-		case 0:
-			g[pos] = 1;
-			break;
-		case 1:
-			g[pos] = g[pos-3] + g[pos+1];
-			break;
-		case 2:
-			g[pos] = g[pos-3] + g[pos+3];
-			break;
-		case 3:
-			g[pos] = g[pos-3] + g[pos-1];
-			break;
-		case 4:
-			g[pos] = g[pos+1] + g[pos+3];
-			break;
-		case 5:
-			g[pos] = g[pos+1] + g[pos-1];
-			break;
-		case 6:
-			g[pos] = g[pos+3] + g[pos-1];
-			break;
-		default:
-			__builtin_unreachable();
-	}
 }
 
 void Grid::legal()
@@ -51,42 +22,34 @@ void Grid::legal()
 	for (int pos = 0; pos < 9; pos++) {
 		if (g[pos])
 			continue;
-		c[pos][0] = true;
-		if (pos >= 3 && pos%3 < 2
-			&& g[pos-3] && g[pos-3] <= 3
-			&& g[pos+1] && g[pos+1] <= 3) {
-			c[pos][0] = false;
-			c[pos][1] = true;
-		}
-		if (pos >= 3 && pos < 6
-			&& g[pos-3] && g[pos-3] <= 3
-			&& g[pos+3] && g[pos+3] <= 3) {
-			c[pos][0] = false;
-			c[pos][2] = true;
-		}
-		if (pos >= 3 && pos%3
-			&& g[pos-3] && g[pos-3] <= 3
-			&& g[pos-1] && g[pos-1] <= 3) {
-			c[pos][0] = false;
-			c[pos][3] = true;
-		}
-		if (pos%3 < 2 && pos < 6
-			&& g[pos+1] && g[pos+1] <= 3
-			&& g[pos+3] && g[pos+3] <= 3) {
-			c[pos][0] = false;
-			c[pos][4] = true;
-		}
-		if (pos%3 < 2 && pos%3
-			&& g[pos+1] && g[pos+1] <= 3
-			&& g[pos-1] && g[pos-1] <= 3) {
-			c[pos][0] = false;
-			c[pos][5] = true;
-		}
-		if (pos < 6 && pos%3
-			&& g[pos+3] && g[pos+3] <= 3
-			&& g[pos-1] && g[pos-1] <= 3) {
-			c[pos][0] = false;
-			c[pos][6] = true;
+		c[pos][0] = 1;
+		for (int capt = 1; capt < 16; capt++) {
+			int res = 0;
+			if (capt & 8) {
+				if (pos < 3 || !g[pos-3])
+					continue;
+				res += g[pos-3];
+			}
+			if (capt & 4) {
+				if (pos%3 == 2 || !g[pos-3])
+					continue;
+				res += g[pos+1];
+			}
+			if (capt & 2) {
+				if (pos >= 6 || !g[pos-3])
+					continue;
+				res += g[pos+3];
+			}
+			if (capt & 1) {
+				if (pos%3 || !g[pos-3])
+					continue;
+				res += g[pos-1];
+			}
+
+			if (res > 6)
+				continue;
+			c[pos][0] = 0;
+			c[pos][capt] = res;
 		}
 	}
 }
